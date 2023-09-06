bd722bbafcb0b8b2681853a5931449d21164600d
Adinarayana Reddy JC <jc.adi101@gmail.com>
2023-09-06 14:10:37 +0530

    Add project files.

---
+++ Jenkins_Tests/JenkinsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jenkins_Services;
+
+namespace Jenkins_Tests
+{
+    [TestClass]
+    public class JenkinsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDataService dataService = new DataService();
+            var hostName = dataService.GetHost();
+            Assert.AreEqual(Environment.MachineName, hostName);
+        }
+    }
+}
